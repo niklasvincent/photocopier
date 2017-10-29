1845c99b8bc3088433a0b9b50789efaf5b279f60
Niklas Lindblad <niklas@lindblad.info>
2017-10-29 15:19:19 +0000

    Improve discovery of Photos library directory

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,8 @@ import datetime
 from photocopier import database
 
 
-def test():
+def test_core_data_timestamp_conversion():
+    """Test core data to UNIX epoch timestamp conversion"""
     ts = database.Database._datetime_from_core_data_timestamp(
         1,
         0
